f9f938adae70a82b6d500a50fe67d1e05c4043de
m.irfan <m.irfanwebdev@gmail.com>
2023-12-13 23:31:11 +0700

    add result car card layout

--- src/pages/Result/index.jsx
+++ src/pages/Result/index.jsx
@@ -1,34 +1,37 @@
-import { useState } from "react"
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react"
+// import { Link } from 'react-router-dom';
+import axios from "axios";
 import Footer from "../../components/Footer";
 import SearchBar from "../../components/SearchBar";
+import './styles.css'
 
 const Result = () => {
     const [name, setName] = useState('');
     const [cars, setCars] = useState([]);
 
-    // useEffect(() => {
-    //     handleGetCar(name, category, isRented)
-    // }, [])
+    useEffect(() => {
+        // handleGetCar(name, category, isRented)
+        handleGetCar()
+    }, [])
 
-    // const handleGetCar = (
-    //     dataName = '', 
-    //     dataCategory = '', 
-    //     dataStatus = '',
-    //     dataMinPrice = '',
-    //     dataMaxPrice = '',
-    // ) => {
-    //     axios
-    //         .get(`https://api-car-rental.binaracademy.org/customer/v2/car?name=${dataName}&category=${dataCategory}&isRented=${dataStatus}&page=1&pageSize=10&minPrice=${dataMinPrice}&maxPrice=${dataMaxPrice}`)
-    //         // 
-    //         .then((res) => {
-    //             const carData = res.data.cars
-    //             setCars(carData)
-    //             // console.log(carData)
-    //             // console.log(dataMinPrice, dataMaxPrice)
-    //         })
-    //         .catch((err) => console.log(err))
-    // }
+    const handleGetCar = (
+        dataName = '', 
+        dataCategory = '', 
+        dataStatus = '',
+        dataMinPrice = '',
+        dataMaxPrice = '',
+    ) => {
+        axios
+            .get(`https://api-car-rental.binaracademy.org/customer/v2/car?name=${dataName}&category=${dataCategory}&isRented=${dataStatus}&page=1&pageSize=10&minPrice=${dataMinPrice}&maxPrice=${dataMaxPrice}`)
+            // 
+            .then((res) => {
+                const carData = res.data.cars
+                setCars(carData)
+                // console.log(carData)
+                // console.log(dataMinPrice, dataMaxPrice)
+            })
+            .catch((err) => console.log(err))
+    }
     
     return (
         <div>
@@ -36,19 +39,20 @@ const Result = () => {
             {/* Search bar */}
             <SearchBar/>
             {/* Search result */}
-            {/* <div className="car-list-container">
+            <div className="car-list-container">
                 {cars.map((item) => (
                     <div key={item.id} className="car-card">
                         <img src={item.image} />
                         <p>{item.name}</p>
                         <h2>Rp. {item.price}/ hari</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <Link to={`/search/${item.id}`}>
+                        {/* <Link to={`/search/${item.id}`}>
                             <button>Pilih Mobil</button>
-                        </Link>
+                        </Link> */}
+                        <button>Pilih Mobil</button>
                     </div>
                 ))}
-            </div> */}
+            </div>
             <Footer/>
         </div>
     )

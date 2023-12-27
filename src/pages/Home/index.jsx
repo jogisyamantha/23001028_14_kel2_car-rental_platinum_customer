5183efdfdc826e60142ae9b4323680a24d30c884
prasetion <prasetion@gmail.com>
2023-12-27 19:34:44 +0700

    modify home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import "./style.css";
+import Footer from "../../components/Footer";
+import WhyUs from "../../components/WhyUs";
+import Banner from "../../components/Banner";
+import FAQ from "../../components/FAQ";
+import OurServices from "../../components/OurServices";
+import Header from "../../components/Header";
+import Testimony from "../../components/Testimony";
 
-const Home = () => {
+const LandingPage = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="landing-page-container">
+      <Header hasBanner={true} />
+      <OurServices />
+      <WhyUs />
+      <Testimony />
+      <Banner />
+      <FAQ />
+      <Footer />
     </div>
   );
 };
 
-export default Home;
+export default LandingPage;

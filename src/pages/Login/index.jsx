3809d9e0a268ff8c5e67b07203a76cbf4d52ecc5
prasetion <prasetion@gmail.com>
2024-01-22 14:12:02 +0700

    login page ui

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { postLogin } from "../../redux/features/loginSlice";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
+import "./style.css";
+import Logo from "../../assets/logo.png";
+import LoginPage from "../../assets/assets-login/landingpage.png";
 
 const Login = () => {
   const [loginPayload, setloginPayload] = useState({
@@ -33,12 +36,52 @@ const Login = () => {
   }, [loginState]);
 
   return (
-    <div>
-      <label>Email:</label>
-      <input name="email" type="text" onChange={handleOnChange} />
-      <label>Password:</label>
-      <input name="password" type="password" onChange={handleOnChange} />
-      <button onClick={handleLogin}>Login</button>
+    <div className="login-container">
+      <div className="left-login">
+        <div className="left-login-container">
+          <div>
+            <img src={Logo} alt="dummy-logo" />
+          </div>
+          <div className="login-title">Welcome Back!</div>
+          <div className="login-form">
+            <div className="login-form-input">
+              <label>Email</label>
+              <input
+                name="email"
+                type="text"
+                placeholder="Contoh: johndee@gmail.com"
+                onChange={handleOnChange}
+              />
+            </div>
+            <div className="login-form-input">
+              <label>Password</label>
+              <input
+                name="password"
+                type="password"
+                placeholder="6+ Karakter"
+                onChange={handleOnChange}
+              />
+            </div>
+          </div>
+          <div>
+            <button onClick={handleLogin}>Sign In</button>
+          </div>
+          <div className="login-register-container">
+            <div>Don't have an account? </div>
+            <div>
+              <Link to={"/register"}>Sign up for free</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="right-login">
+        <div className="right-login-container">
+          <div className="right-login-title">Binar Car Rental</div>
+          <div>
+            <img src={LoginPage} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

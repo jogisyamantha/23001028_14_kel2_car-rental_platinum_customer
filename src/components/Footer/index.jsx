f77eeaefc7d88ee717b56581660831b0884a5e17
prasetion <prasetion@gmail.com>
2024-01-23 23:56:49 +0700

    fix accordion, fix footer, fix layout

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,40 +1,41 @@
-import facebook from '../../assets/social-media/icon_facebook.png'
-import instagram from '../../assets/social-media/icon_instagram.png'
-import mail from '../../assets/social-media/icon_mail.png'
-import twitch from '../../assets/social-media/icon_twitch.png'
-import twitter from '../../assets/social-media/icon_twitter.png'
-import './styles.css'
+import facebook from "../../assets/social-media/icon_facebook.png";
+import instagram from "../../assets/social-media/icon_instagram.png";
+import mail from "../../assets/social-media/icon_mail.png";
+import twitch from "../../assets/social-media/icon_twitch.png";
+import twitter from "../../assets/social-media/icon_twitter.png";
+import "./styles.css";
+import Logo from "../../assets/logo.png";
 
 const Footer = () => {
-    return (
-        <div className='footer'>
-            <div className="address">
-                <p>Jalan Suroyo No. 161 Mayangan Kota Probolonggo 672000</p>
-                <p>binarcarrental@gmail.com</p>
-                <p>081-233-334-808</p>
-            </div>
-            <div className="footer-navigation">
-                <p>Our services</p>
-                <p>Why Us</p>
-                <p>Testimonial</p>
-                <p>Testimonial</p>
-                <p>FAQ</p>
-            </div>
-            <div className="social-media">
-                <p>Connect with us</p>
-                <div>
-                    <img src={facebook} alt='facebook' />
-                    <img src={instagram} alt='instagram' />
-                    <img src={twitter} alt='twitter' />
-                    <img src={mail} alt='mail' />
-                    <img src={twitch} alt='twitch' />
-                </div>
-            </div>
-            <div className="copyright">
-                <p>Copyright Binar 2022</p>
-            </div>
+  return (
+    <div className="footer">
+      <div className="address">
+        <p>Jalan Suroyo No. 161 Mayangan Kota Probolonggo 672000</p>
+        <p>binarcarrental@gmail.com</p>
+        <p>081-233-334-808</p>
+      </div>
+      <div className="footer-navigation">
+        <p>Our services</p>
+        <p>Why Us</p>
+        <p>Testimonial</p>
+        <p>FAQ</p>
+      </div>
+      <div className="social-media">
+        <p>Connect with us</p>
+        <div>
+          <img src={facebook} alt="facebook" />
+          <img src={instagram} alt="instagram" />
+          <img src={twitter} alt="twitter" />
+          <img src={mail} alt="mail" />
+          <img src={twitch} alt="twitch" />
         </div>
-    )
-}
+      </div>
+      <div className="copyright">
+        <p>Copyright Binar 2022</p>
+        <img src={Logo} alt="" />
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
--- src/components/WhyUs/index.jsx
+++ src/components/WhyUs/index.jsx
@@ -4,12 +4,28 @@ import "./style.css";
 
 const WhyUs = () => {
   const whyUsData = [
-    "Sewa Mobil Dengan Supir di Bali 12 Jam",
-    "Sewa Mobil Lepas Kunci di Bali 24 Jam",
-    "Sewa Mobil Jangka Panjang Bulanan",
-    "Gratis Antar - Jemput Mobil di Bandara",
-    "Layanan Airport Transfer / Drop In Out",
+    {
+      icon: "../../src/assets/assets-homepage/icon_complete.png",
+      title: "Mobil Lengkap",
+      desc: "Tersedia banyak pilihan mobil, kondisi masih baru, bersih dan terawat",
+    },
+    {
+      icon: "../../src/assets/assets-homepage/icon_professional.png",
+      title: "Harga Murah",
+      desc: "Harga murah dan bersaing, bisa bandingkan harga kami dengan rental mobil lain",
+    },
+    {
+      icon: "../../src/assets/assets-homepage/icon_24hrs.png",
+      title: "Layanan 24 Jam",
+      desc: "Siap melayani kebutuhan Anda selama 24 jam nonstop. Kami juga tersedia di akhir minggu",
+    },
+    {
+      icon: "../../src/assets/assets-homepage/icon_professional.png",
+      title: "Sopir Profesional",
+      desc: "Sopir yang profesional, berpengalaman, jujur, ramah dan selalu tepat waktu",
+    },
   ];
+
   return (
     <div id="why-us" className="whyus-container container-section">
       <div className="whyus-header">Why Us?</div>

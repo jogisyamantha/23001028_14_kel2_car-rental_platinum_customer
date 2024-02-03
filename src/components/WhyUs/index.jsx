35de71747d1f2825672fb61f3817c665c068aa15
prasetion <prasetion@gmail.com>
2024-02-03 08:52:45 +0700

    fixing image whyus

--- src/components/WhyUs/index.jsx
+++ src/components/WhyUs/index.jsx
@@ -5,22 +5,22 @@ import "./style.css";
 const WhyUs = () => {
   const whyUsData = [
     {
-      icon: "../../src/assets/assets-homepage/icon_complete.png",
+      icon: "../../../src/assets/assets-homepage/icon_complete.png",
       title: "Mobil Lengkap",
       desc: "Tersedia banyak pilihan mobil, kondisi masih baru, bersih dan terawat",
     },
     {
-      icon: "../../src/assets/assets-homepage/icon_professional.png",
+      icon: "../../../src/assets/assets-homepage/icon_professional.png",
       title: "Harga Murah",
       desc: "Harga murah dan bersaing, bisa bandingkan harga kami dengan rental mobil lain",
     },
     {
-      icon: "../../src/assets/assets-homepage/icon_24hrs.png",
+      icon: "../../../src/assets/assets-homepage/icon_24hrs.png",
       title: "Layanan 24 Jam",
       desc: "Siap melayani kebutuhan Anda selama 24 jam nonstop. Kami juga tersedia di akhir minggu",
     },
     {
-      icon: "../../src/assets/assets-homepage/icon_professional.png",
+      icon: "../../../src/assets/assets-homepage/icon_professional.png",
       title: "Sopir Profesional",
       desc: "Sopir yang profesional, berpengalaman, jujur, ramah dan selalu tepat waktu",
     },
